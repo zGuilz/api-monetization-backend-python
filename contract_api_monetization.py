e656b7bcb89944b75bd484e4354fe8ce715f3546
zGuilz <guilhermeferreiraaraujo@hotmail.com>
2022-07-16 23:33:50 -0300

    add new codes

--- contract_api_monetization.py
+++ contract_api_monetization.py
@@ -6,15 +6,25 @@ from connect_db import conecta_db
 
 class ContractApiMonetization():
 
-    max_quantity_contract = 0
-    counter_requests = 0
-    api_name = ""
+    def get_id_by_name(self, name):
+        #Pesquisa no banco de dados por name e devolve um contrato de api
+        db = conecta_db()
+        connection = db.cursor()
+        query = f"""
+        SELECT * FROM "public"."contract_api_monetization" WHERE name = '{name}'
+        """
+        try:
+            connection.execute(query)
+            result = connection.fetchone()
+            connection.close()
+            return result[0] #id
+
+        except Exception as e:
+            print(f"Exception: {e}")
+            db.rollback()
+            connection.close()
     
-
-    def __init__(self, max_quantity_contract, api_name):
-        self.max_quantity_contract = max_quantity_contract
-        self.api_name = api_name
-
+    
     def get_contract_by_id(self, id):
         #Pesquisa no banco de dados por id e devolve um contrato de api
         db = conecta_db()
@@ -24,7 +34,7 @@ class ContractApiMonetization():
         """
         try:
             connection.execute(query)
-            result = connection.fetchall()
+            result = connection.fetchone()
             connection.close()
             return result
 
@@ -62,26 +72,73 @@ class ContractApiMonetization():
         """
         try:
             connection.execute(query)
-            result = connection.fetchall()
+            result = connection.fetchone()
             connection.close()
-            return result[0][0] #Tupla dentro de uma lista
+            return result[0] #Tupla dentro de uma lista
 
         except Exception as e:
             print(f"Exception: {e}")
             db.rollback()
             connection.close()
     
-    def get_counter_requests_by_id(self, id, api, route):
+    def redirect_api_by_id(self, id):
         #Pesquisa no banco quantas requisições já foram feitas
-        counter_requests_by_id = 10 #Vai mudar com o retorno do banco
+        contract = self.get_contract_by_id(id)
+        counter_requests_by_id = contract[5] #Counter Requests
+
         max_quantity_contract = self.get_max_quantity_contract_by_id(id)
 
         if counter_requests_by_id >= max_quantity_contract:
             # Retorna um 400 ou outro Status code Descrevendo que atingiu o limite
+            print("Request Limit")
             return "Request Limit"
         else:
-            # Efetua o Redirect para a API e Rota solicitada
+            # Efetua o Redirect para a API e Rota solicitada e inseri no contador +1
+            query = f"""
+            UPDATE contract_api_monetization
+            SET counter_requests = {counter_requests_by_id + 1}
+            WHERE id = {id};
+            """
+            db = conecta_db()
+            connection = db.cursor()
+            
+            try:
+                connection.execute(query)
+                db.commit()
+                connection.close()
+
+            except Exception as e:
+                print(f"Exception: {e}")
+                db.rollback()
+                connection.close()
+
+
             return requests.get("https://www.google.com").content
 
-test = ContractApiMonetization(100, "Dummy")
-print(test.get_max_quantity_contract_by_id(2))
+    def create_contract(self, name, max_quantity_contract, amount, cryptocurrency, created_on, company):
+        query = f"""
+        INSERT INTO contract_api_monetization(name, max_quantity_contract, amount, cryptocurrency, counter_requests, created_on, company)
+        VALUES ('{name}', {max_quantity_contract}, {amount}, '{cryptocurrency}', 0, '{created_on}', '{company}');
+        """
+
+        db = conecta_db()
+        connection = db.cursor()
+            
+        try:
+            connection.execute(query)
+            db.commit()
+            connection.close()
+
+            return "Contract Create Success"
+
+        except Exception as e:
+            print(f"Exception: {e}")
+            db.rollback()
+            connection.close()
+
+            return "Contract Create Erro"
+
+
+test = ContractApiMonetization()
+
+print(test.redirect_api_by_id(3))
